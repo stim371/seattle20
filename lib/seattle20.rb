710642863bce511fbc12344537821316066e521c
Joel Stimson <stimsojr@gmail.com>
2011-12-14 00:58:38 -0800

    cleaned up ternary operator assignment and moved the encoding into the rest of the response stuff. next need to convert output to hash

--- lib/seattle20.rb
+++ lib/seattle20.rb
@@ -3,21 +3,18 @@ require 'net/http'
 
 class Seattle20
   BASEURL = "http://www.seattle20.com/api/v1/"
-  url_end = ""
   
   def self.events(params={})
     short_url = BASEURL + "get_events.aspx"
-    params.empty? ? url = short_url : url = short_url + "?" + params.map { |key,val| key.to_s + "=" + val.to_s }.join("&")
-    url = URI.encode(short_url)
-    response = Net::HTTP.get_response(URI.parse(url))
+    url = params.empty? ? short_url : short_url + "?" + params.map { |key,val| key.to_s + "=" + val.to_s }.join("&")
+    response = Net::HTTP.get_response(URI.parse(URI.encode(url)))
     response.body
   end
   
   def self.ssi(params={})
     short_url = BASEURL + "get_ssi.aspx"
-    params.empty? ? url = short_url : url = short_url + "?" + params.map { |key,val| key.to_s + "=" + val.to_s }.join("&")
-    url = URI.encode(short_url)
-    response = Net::HTTP.get_response(URI.parse(url))
+    url = params.empty? ? short_url : short_url + "?" + params.map { |key,val| key.to_s + "=" + val.to_s }.join("&")
+    response = Net::HTTP.get_response(URI.parse(URI.encode(url)))
     response.body
   end
   
