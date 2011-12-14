1ed049b7bb495ae6035b5bec2adc5e444b1389c2
Joel Stimson <stimsojr@gmail.com>
2011-12-14 00:46:56 -0800

    added ability to grab params and concat on the end of the url.

--- lib/seattle20.rb
+++ lib/seattle20.rb
@@ -3,16 +3,19 @@ require 'net/http'
 
 class Seattle20
   BASEURL = "http://www.seattle20.com/api/v1/"
+  url_end = ""
   
-  def self.events
+  def self.events(params={})
     short_url = BASEURL + "get_events.aspx"
+    params.empty? ? url = short_url : url = short_url + "?" + params.map { |key,val| key.to_s + "=" + val.to_s }.join("&")
     url = URI.encode(short_url)
     response = Net::HTTP.get_response(URI.parse(url))
     response.body
   end
   
-  def self.ssi
+  def self.ssi(params={})
     short_url = BASEURL + "get_ssi.aspx"
+    params.empty? ? url = short_url : url = short_url + "?" + params.map { |key,val| key.to_s + "=" + val.to_s }.join("&")
     url = URI.encode(short_url)
     response = Net::HTTP.get_response(URI.parse(url))
     response.body
